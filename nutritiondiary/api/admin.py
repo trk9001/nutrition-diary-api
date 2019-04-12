1d7dd0f0c6d058b790a74756dc56829085bfa285
trk <dev.trk.9001@gmail.com>
2019-04-12 23:47:40 +0600

    Customize the admin site a little

--- nutritiondiary/api/admin.py
+++ nutritiondiary/api/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from . import models
 
+admin.site.site_header = 'Nutrition Diary API Administration'
+admin.site.site_title = 'Nutrition Diary API'
+admin.site.index_title = 'Admin'
+
 admin.site.register(models.NutritionData)
