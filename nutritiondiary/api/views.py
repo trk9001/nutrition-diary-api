1ec127066f225a3d01dbd5fad45e2c7cd66d35b6
trk <dev.trk.9001@gmail.com>
2019-04-12 22:28:25 +0600

    Create an API view listing nutrition data

--- nutritiondiary/api/views.py
+++ nutritiondiary/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import filters, generics, views
 
-# Create your views here.
+from . import models
+from . import serializers
+
+
+class NutritionDataView(generics.ListAPIView):
+    """API view listing nutrition data."""
+
+    queryset = models.NutritionData.objects.all()
+    serializer_class = serializers.NutritionDataSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['item_name']
